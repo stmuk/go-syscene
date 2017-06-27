9e768239827761057138b8293bb0e9f85450be15
Steve Mynott <steve.mynott@gmail.com>
2017-06-27 20:31:48 +0100

    crude caching

--- gosearch.go
+++ gosearch.go
@@ -3,16 +3,34 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
+
+	if len(os.Args) == 1 {
+		panic("must pass search target as arg")
+	}
+
+	target := os.Args[1]
+
+	if rand.Intn(100) == 100 {
+		resp, _ := http.Get("http://go-search.org/api?action=packages")
+
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		_ = ioutil.WriteFile("packages.data", body, 0644)
+	}
+
 	packages, _ := ioutil.ReadFile("packages.data")
 	data := strings.Split(string(packages), ",")
 
 	for _, item := range data {
-		if strings.Contains(item, os.Args[1]) {
+		if strings.Contains(item, target) {
 			fmt.Println(item)
 		}
 	}
